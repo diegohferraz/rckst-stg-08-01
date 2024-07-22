7032cca56efd9588b9ac7a85fbdf0365cb7faacf
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2024-07-22 11:56:35 -0300

    feat: adds GET example with Hello World

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3333;
 
+app.get("/", (request, response) => {
+  response.send("Hello world!");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
 });
