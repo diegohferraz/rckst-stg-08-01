463df69da0755557fbfe5545d883280e08c6d44d
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2024-07-24 14:50:57 -0300

    feat: adds create notes with tags and links features

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,28 +1,19 @@
 const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
-const sqliteConnection = require("../databases/sqlite");
+const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-  /**
-   * index - GET para listar varios registros
-   * show - GET par aum registro especifico
-   * create - POST para criar um registro
-   * update - PUT para atualizar um registro
-   * delete - DELETE para remover um registro
-   */
-
   async create(request, response) {
     const { name, email, password } = request.body;
 
     const database = await sqliteConnection();
-
-    const checkUserExist = await database.get(
+    const checkUserExists = await database.get(
       "SELECT * FROM users WHERE email = (?)",
       [email]
     );
 
-    if (checkUserExist) {
-      throw new AppError("Este email ja esta em uso");
+    if (checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.");
     }
 
     const hashedPassword = await hash(password, 8);
@@ -32,7 +23,7 @@ class UsersController {
       [name, email, hashedPassword]
     );
 
-    response.status(201).json();
+    return response.status(201).json();
   }
 
   async update(request, response) {
