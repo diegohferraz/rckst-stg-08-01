669825e35eb6afac3e5fc26c3ef7a87b2b8380ae
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2024-07-25 17:02:40 -0300

    feat: adds session controller and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,13 @@ const { Router } = require("express");
 const usersRoutes = require("./user.routes");
 const notesRoutes = require("./notes.routes");
 const tagsRoutes = require("./tags.routes");
+const sessionRoutes = require("./sessions.routes");
 
 const routes = Router();
+
 routes.use("/users", usersRoutes);
 routes.use("/notes", notesRoutes);
 routes.use("/tags", tagsRoutes);
+routes.use("/session", sessionRoutes);
 
 module.exports = routes;
